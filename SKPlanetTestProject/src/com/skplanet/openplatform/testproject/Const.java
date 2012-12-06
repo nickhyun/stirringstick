ec52a372eb3e47ac5ab13193e719d358c3fb9f3d
nickhyun <nickhyun@gmail.com>
2012-12-06 14:08:30 +0900

    InitialDraft

---
+++ SKPlanetTestProject/src/com/skplanet/openplatform/testproject/Const.java
@@ -0,0 +1,6 @@
+package com.skplanet.openplatform.testproject;
+
+public class Const {
+	public static String SERVER = "https://apis.skplanetx.com";
+	public static String SERVER_PUBLIC = "http://apis.skplanetx.com";
+}
---
+++ SKPlanetTestProject/src/com/skplanet/openplatform/testproject/Init.java
@@ -0,0 +1,34 @@
+package com.skplanet.openplatform.testproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.skp.openplatform.android.sdk.common.BaseActivity;
+import com.skp.openplatform.android.sdk.oauth.OAuthInfoManager;
+
+public class Init extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		initOAuthData();
+		moveToMainPage();
+	}
+
+	public void initOAuthData() {
+
+		// Input here.
+		new OAuthInfoManager("##APPKEY_INPUTHERE##");
+		
+		OAuthInfoManager.clientId = "##CLIENTID_INPUTHERE##";
+		OAuthInfoManager.clientSecret = "##CLIENTSECRET_INPUTHERE##";
+		OAuthInfoManager.scope = "##SCOPE_INPUTHERE##";
+	}
+	
+	public void moveToMainPage() {
+		startActivity(new Intent(this, Main.class));
+		finish();
+	}
+
+}
